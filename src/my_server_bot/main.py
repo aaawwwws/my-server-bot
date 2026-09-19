e196f25699992105362f3db120cd6df95dfca248
aaawwwws <89956317+aaawwwws@users.noreply.github.com>
2026-09-19 17:31:35 +0900

    Initial commit with private email config

---
+++ src/my_server_bot/main.py
@@ -0,0 +1,51 @@
+import os
+
+import discord
+from discord.types.channel import TextChannel
+from dotenv import load_dotenv
+
+
+def main() -> int:
+    load_dotenv()
+    TOKEN = os.getenv("TOKEN")
+    intents = discord.Intents.all()
+    bot = discord.Bot(intents=intents)
+
+    @bot.event
+    async def on_ready():
+        activity = discord.Game(name="CIV6をプレイ中と")
+        await bot.change_presence(activity=activity)
+        print("ready!")
+
+    @bot.event
+    async def on_message(message: discord.Message):
+        # メッセージ送信者がBot(自分を含む)だった場合は無視する
+        if message.author.bot:
+            return
+
+        # 招待コードの場合削除して招待コードチャンネルに再投稿
+        if (
+            "Steam://" in message.content
+            or message.content.isdigit()
+            and len(message.content) == 6
+        ):
+            await repost(message, "招待コード")
+
+        # Steamリンクの場合、削除してSteamリンクチャンネルに再投稿
+        if "https://store.steampowered.com/app/" in message.content:
+            await repost(message, "steamリンク")
+
+    bot.run(TOKEN)
+    return 0
+
+
+async def repost(message: discord.Message, channelName: str):
+    # サーバーでの発言以外は無視する
+    guild = message.guild
+    if guild == None:
+        return
+
+    channel = discord.utils.find(lambda c: c.name == channelName, guild.channels)
+    if isinstance(channel, discord.TextChannel):
+        await channel.send(message.content)
+        await message.delete()
